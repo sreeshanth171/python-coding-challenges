fc1b12a065f326471d9818c15be11d2e84f57d13
sreeshanth cv <sreeshanthcv123@gmail.com>
2026-08-21 11:07:40 +0530

    Added Week 03 stone paper scissor challenge

--- week-03/stone_paper_scissor.py
+++ week-03/stone_paper_scissor.py
@@ -1,1 +1,12 @@
-
+import random
+list = ["stone","paper","scissor"]
+choise = input("enter your choice : ").lower()
+computer = random.choice(list)
+if choise == computer:
+    print("draw","computer choose ",computer)
+elif (computer == "scissor" and choise == "paper") or \
+    (computer == "stone" and choise == "scissor") or \
+    (computer == "paper" and choise == "stone"):
+    print("you lose ","computer choose ",computer)
+else:
+    print("you win computer choose ",computer)
